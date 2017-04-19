b0625aa93a70cb337143c09682ab9f3af4d4203a
Алексеева Анастасия Николаевна <analekseeva_1@edu.hse.ru>
2017-04-19 15:05:30 +0500

    хех

---
+++ Task1/Point.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Point // однонаправленный список
+    {
+        public byte data;
+        public Point next;
+
+        public byte Number
+        {
+            get { return data; }
+            set
+            {
+                data = (byte)(value % 10);
+                if (next == null)
+                    next = new Point(0);
+                next.Number += (byte) (value / 10);
+            }
+        }
+
+        public Point(byte d)
+        {
+            data = d;
+            next = null;
+        }
+
+        public Point this[int i]
+        {
+            get
+            {
+                var p = this;
+                if (i == 0)
+                    return p;
+                return next?[i - 1];
+            }
+        }
+        public int length
+        {
+            get
+            {
+                var p = this;
+                int r = 0;
+                while (p != null)
+                {
+                    r++;
+                    p = p.next;
+                }
+                return r;
+            }
+        }
+
+        public static Point operator +(Point right, Point left)
+        {
+            var result = new Point(0);
+            for (int i = 0; i < Math.Min(left.length, right.length); i++)
+            {
+                result[i].Number = (byte)(right[i].Number + left[i].Number);
+            }
+            return result;
+        }
+
+        public Point getLast()
+        {
+            var p = this;
+            while (p.next != null)
+                p = p.next;
+            return p;
+        }
+        public void add(byte p)
+        {
+            var b = getLast();
+            b.next = new Point(1);
+        }
+
+        public override string ToString()
+        {
+            var p = this;
+            string result = string.Empty;
+            while (p != null)
+            {
+                result = p.data + result;
+                p = p.next;
+            }
+            return result;
+        }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task1
 {
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.Write("Input n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            long a = 1, b = 1, r = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                r = a + b;
+                b = a;
+                a = r;
+            }
+            long result = (long) Math.Pow(r, 2);
+            Console.WriteLine(r);
+            Console.WriteLine("result = {0}", result);
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+
         }
     }
 }
