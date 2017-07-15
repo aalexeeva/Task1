497248a4a55f5c179ee3f5591a6104061f3d4a95
Алексеева Анастасия Николаевна <analekseeva_1@edu.hse.ru>
2017-07-15 12:55:16 +0500

    задание выполнено. код оптимизирован

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
+using static System.Console;
 
 namespace Task1
 {
-
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.Write("Input n: ");
-            int n = Convert.ToInt32(Console.ReadLine());
+            Write("Input n: ");
+            int n = Convert.ToInt32(ReadLine());
             long a = 1, b = 1, r = 0;
             for (int i = 0; i < n - 1; i++)
             {
@@ -17,11 +17,10 @@ namespace Task1
                 a = r;
             }
             long result = (long) Math.Pow(r, 2);
-            Console.WriteLine(r);
-            Console.WriteLine("result = {0}", result);
-            Console.Write("Press any key to continue . . . ");
-            Console.ReadKey(true);
-
+            WriteLine(r);
+            WriteLine("result = {0}", result);
+            Write("Press any key to continue . . . ");
+            ReadKey(true);
         }
     }
 }
